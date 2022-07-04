bc358c82c85f8ac54b83678de27608e162d48a35
Taphline <taphlinem20@gmail.com>
2022-07-04 09:37:30 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+	return (0);
+}
