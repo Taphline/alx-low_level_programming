a0461f872ec94874f082a2cb4d118490cab5d1c7
Taphline <taphlinem20@gmail.com>
2022-07-06 09:45:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
- * main -prints to console
- *
- * Description: prints _putchar string to console
- *
- * Return: Always 0 (Success)
- */
+* main -prints to console
+* Description: prints _putchar string to console
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char text[] = "_putchar";
-	int i=0;
+	int i = 0;
 
 	while (text[i] != 0)
 	{
-		_putchar(text[i];
+		_putchar(text[i]);
 		i++;
 	}
 	_putchar('\n');
-
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -5,9 +8,15 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
+int print_last_digit(int); 
+void jack_bauer(void); 
+void times_table(void); 
+int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
