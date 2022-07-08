1c4a1af3c45b27d07228fe776b8dfacf6aff50d3
Taphline <taphlinem20@gmail.com>
2022-07-07 20:03:37 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,6 +11,7 @@ int main(void)
 	unsigned long num = 612852475143;
 
 	unsigned long div = 2;
+
 	while (div < num)
 	{
 		if (num % div == 0)
@@ -20,6 +21,5 @@ int main(void)
 	}
 
 	printf("%lu\n", num);
-
 	return (0);
 }
