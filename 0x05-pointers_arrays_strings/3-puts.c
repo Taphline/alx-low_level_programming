4ea28b86f615d5fe4fb58e11b068e34b9f9b5edd
Taphline <taphlinem20@gmail.com>
2022-07-11 10:15:12 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _puts -prints a string to stdout
+* @str: input string
+*
+* Return: Always 0
+*/
+void _puts(char *str)
+{
+	str = (char *)str;
+	{
+		puts(str);
+		_putchar(str);
+	{
+	_putchar('\n');
+}
