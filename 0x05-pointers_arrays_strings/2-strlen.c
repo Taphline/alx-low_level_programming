fb16f86108ec04bb3e73b144ff083fb38b566da9
Taphline <taphlinem20@gmail.com>
2022-07-11 09:31:16 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* _strlen - get a string length
+* @s: input string
+*
+* Return: integer len
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	s = (char *) s;
+	a = strlen(s);
+
+	return (a);
+}
