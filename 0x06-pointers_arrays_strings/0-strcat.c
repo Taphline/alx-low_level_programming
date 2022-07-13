339c70c23c188831013ec96931aa2d4537d5a92c
Taphline <taphlinem20@gmail.com>
2022-07-13 08:01:34 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* char *_strcat -function that concatenates two strings.
+* @dest: the destination of the string
+* @src: the string to be appended
+*
+* Return: Always 0
+*/
+char *_strcat(char *dest, char *src)
+{
+	char *p ;
+
+	strcat(dest, src);
+	p = dest;
+
+	return (p);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!bin/bash
+This is a readme file for more pointers arrays and strings
