e56c59106f5ac8477cf2f104af1cccca8e1e285a
Taphline <taphlinem20@gmail.com>
2022-07-18 09:56:07 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*_memcpy -copies memory area.
+*@dest: character destination
+*@src:character source
+*@n:integer
+*
+*Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d;
+
+	_memcpy(dest, src, n);
+	d = (dest);
+
+	return (dest);
+}
