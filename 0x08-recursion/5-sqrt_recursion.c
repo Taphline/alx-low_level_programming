bef5c53d4ff5aa38997ba4d3fbe3e107a13b4425
Taphline <taphlinem20@gmail.com>
2022-07-20 15:57:17 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
 *_sqrt_recursion - returns natural square root of a number
 *@n: Integer number
-*
 *Return: Square root
 */
 int _sqrt_recursion(int n)
@@ -13,16 +11,13 @@ int _sqrt_recursion(int n)
 	if (n == 1)
 		return (1);
 	if (n < 0)
-		return (ERROR);
-	return (NumSqr(n, 1));
+		return (-1);
+	return (NumSqrt(n, 1));
 }
-
 /**
 *NumSqrt - function taht gets the number of sqroot
-*
 *@num:Input value
 *@i: Counter variable
-*
 *Return: int 
 */
 int NumSqrt(int num, int i)
@@ -30,7 +25,7 @@ int NumSqrt(int num, int i)
 	if (num == (i * i))
 		return (i);
 	else if (num > (i * i))
-		return (Numsqrt(num, i + 1));
+		return (NumSqrt(num, i + 1));
 	else
-		return (ERROR);
+		return (-1);
 }
