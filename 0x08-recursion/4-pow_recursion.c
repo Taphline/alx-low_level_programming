d4cae830510a0a75bc3cfad997bae7ab70d5c3af
Taphline <taphlinem20@gmail.com>
2022-07-20 15:50:35 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,20 +4,17 @@
 *_pow_recursion - returns power of a number
 *@x: Integer number
 *@y: Integer number
-*
 *Return: power of a number
 */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
-		return (ERROR);
+		return (-1);
 	}
-	else
+	if (y == 0)
 	{
-		if (y > 0)
-			return (x * ( _pow_recursion(x, y - 1)));
-		else
-			return (1);
+		return (1);
 	}
+	return (x * _pow_recursion(x, y - 1));
 }
