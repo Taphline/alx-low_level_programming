1d21a9c2fa01cc9ea2cc3e0eaeaf58211ce9497c
Taphline <taphlinem20@gmail.com>
2022-07-25 08:45:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_array/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/** 
+* main - Entry point
+* @argc: number of params
+* @argv: array of params
+*
+*Return: Always: 0
+*/
+
+int main(int argc, _noerr, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
